dd383884e3087323e51d820e2a0ddff21634366e
Mitul <mitul@webydo.com>
2024-08-07 13:16:29 +0530

    create component and initial

---
+++ angular_18_demo/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,AddEmployeeComponent,EmployeeListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular_18_demo';
+  sub_title ="learning an angular 18";
+}
